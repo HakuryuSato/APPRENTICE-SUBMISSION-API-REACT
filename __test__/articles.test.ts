e44e954d3ffd28cb4dad04af156e341f57983ee7
HakuryuSato <r700540@gmail.com>
2024-09-19 17:57:36 +0900

    ブランチ追加前

---
+++ __test__/articles.test.ts
@@ -0,0 +1,17 @@
+// import handler from '@/app/api/articles/[slug]';
+
+// describe('/api/articles/[slug]', () => {
+//   it('GET should return an article', async () => {
+//     const { req, res } = createMocks({
+//       method: 'GET',
+//       query: {
+//         slug: 'test-article',
+//       },
+//     });
+
+//     await handler(req, res);
+
+//     expect(res._getStatusCode()).toBe(200);
+//     expect(JSON.parse(res._getData()).article.slug).toBe('test-article');
+//   });
+// });
