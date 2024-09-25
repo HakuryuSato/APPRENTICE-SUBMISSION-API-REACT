243906a5222012dae030f874490b4796e7909d22
HakuryuSato <r700540@gmail.com>
2024-09-25 19:34:30 +0900

    共通部分作成

---
+++ app/components/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = ({ children }: { children: ReactNode }) => (
+  <>
+    <Header />
+    {children}
+    <Footer />
+  </>
+);
+
+export default Layout;
