ce2952944901c2649f8a018c15be915e027a4d97
HakuryuSato <r700540@gmail.com>
2024-09-29 19:09:44 +0900

    remotePatternsの位置を修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['i.imgur.com', 'api.realworld.io'],
+    domains: ["i.imgur.com", "api.realworld.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'i.imgur.com',
-      port: '',
-      pathname: '/**',
-    },
-  ]
-
 };
 
 export default nextConfig;
