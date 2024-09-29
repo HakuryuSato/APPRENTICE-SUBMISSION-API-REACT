ac5b838ae9290b9711d3dce99d61dee1bdd728c3
HakuryuSato <r700540@gmail.com>
2024-09-29 16:33:11 +0900

    イメージ用画像のhostnameを追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['i.imgur.com'],
-    domains: ['api.realworld.io'],
+    domains: ['i.imgur.com', 'api.realworld.io'],
   },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'i.imgur.com',
+      port: '',
+      pathname: '/**',
+    },
+  ]
+
 };
 
 export default nextConfig;
